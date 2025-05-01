71f5e13d5d571af9e8bcb01d58ae8e1c8027f74a
Alvin Odhi N <ucokkocu411@gmail.com>
2025-05-01 22:58:16 +0700

    feat: mege feature-1
    - fix middleware after change password
    -  removing layou.copy.tsx
    - update .env.example
    - refactor user create on user management

--- apps/api/src/repositories/userManagement.repository.ts
+++ apps/api/src/repositories/userManagement.repository.ts
@@ -1,60 +1,87 @@
+import { auth } from '@/auth';
 import { pagination } from '@/helpers/pagination';
 import { User } from '@/interfaces/userManagement.interface';
 import { prismaclient } from '@/prisma';
 
+class UserManagementRepository {
+  async getUsers(page = 1, take = 1) {
+    const total = await prismaclient.user.count();
+    const { skip, take: realTake } = pagination(page, take);
+    const data = await prismaclient.user.findMany({
+      include: {
+        addresses: true,
+        stores: true,
+      },
+      skip,
+      take: realTake,
+    });
 
-class UserManagementRepository{
-    async getUsers(page=1,take=1){
-        const total = await prismaclient.user.count()
-         const {skip,take:realTake} = pagination(page,take)
-                const data= await prismaclient.user.findMany({
-                    include:{
-                        addresses:true,
-                        Store:true
-                    },
-                    skip,
-                    take:realTake
-                })
-        
-                return{total,data}
-        
-       
+    return { total, data };
+  }
+
+  async createUser(userData: User) {
+    const existingUser = await prismaclient.user.findUnique({
+      where: {
+        email: userData.email,
+      },
+    });
+
+    if (existingUser) {
+      const error = new Error('Email already exist');
+      error.name = 'DuplicateEmailError';
+      throw error;
     }
 
-    async createUser(userData:User){
-
-        const existingUser = await prismaclient.user.findUnique({
-            where:{
-                email:userData.email
-            }
-        })
-
-        if(existingUser){
-            const error = new Error('Email already exist')
-            error.name = 'DuplicateEmailError'
-            throw error
-        }
-        return await prismaclient.user.create({
-            data:userData
-        })
+    if (!userData.password) {
+      const error = new Error('Password not provided');
+      error.name = 'DuplicateEmailError';
+      throw error;
     }
 
-    async updateUser(id:string,userData:User){
-        return await prismaclient.user.update({
-            where:{
-                id
-            },
-            data:{
-                ...userData
-            }
-        })
-    }
-    
-    async deleteUser(id:string){
-        return await prismaclient.user.delete({
-            where:{id}
-        })
-    }
+    const ctx = await auth.$context;
+    const { password, ...data } = userData;
+
+    const user = await prismaclient.user.create({
+      data: {
+        email: data.email,
+        emailVerified: true,
+        name: data.name,
+        role: 'ADMIN',
+        signupMethod: {
+          set: ['CREDENTIAL'],
+        },
+      },
+    });
+
+    // creating account
+    await prismaclient.account.create({
+      data: {
+        accountId: user.id,
+        userId: user.id,
+        providerId: 'credential',
+        password: await ctx.password.hash(password),
+      },
+    });
+
+    return user;
+  }
+
+  async updateUser(id: string, userData: User) {
+    return await prismaclient.user.update({
+      where: {
+        id,
+      },
+      data: {
+        ...userData,
+      },
+    });
+  }
+
+  async deleteUser(id: string) {
+    return await prismaclient.user.delete({
+      where: { id },
+    });
+  }
 }
 
-export default  new UserManagementRepository()
+export default new UserManagementRepository();
