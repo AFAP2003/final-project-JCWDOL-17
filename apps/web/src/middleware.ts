7916822f9421001dddd6f3639d251cdd66b8c2b7
Alvin Odhi N <ucokkocu411@gmail.com>
2025-05-01 21:42:52 +0700

    fix: update middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -46,63 +46,40 @@ export async function middleware(request: NextRequest) {
   }
 
   if (isAuthenticated) {
-    try {
-      const { data: session, error } = await betterFetch<
-        typeof authClient.$Infer.Session
-      >('/api/better/auth/get-session', {
-        baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
-        headers: {
-          cookie: request.headers.get('cookie') || '', // Forward the cookies from the request
-        },
-      });
+    const { data: session, error } = await betterFetch<
+      typeof authClient.$Infer.Session
+    >('/api/better/auth/get-session', {
+      baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+      headers: {
+        cookie: request.headers.get('cookie') || '', // Forward the cookies from the request
+      },
+    });
 
-      if (error) {
-        console.error('Session fetch error:', error);
-        return NextResponse.error(); // This triggers the default Next.js error page (500 error)
-      }
+    if (error) {
+      console.error('Session fetch error:', error);
+      return NextResponse.error(); // This triggers the default Next.js error page (500 error)
+    }
+    const { user } = session as unknown as Session;
 
-      // Posible case when user revoke other session, or reset password
-      if (!session) {
-        const url = request.nextUrl.clone();
-        if (currentPath.startsWith('/admin')) {
+    const role = user.role;
+    switch (role) {
+      case 'USER': {
+        if (currentPath.startsWith('/admin') && !pathIsPublic) {
+          const url = request.nextUrl.clone();
           url.pathname = '/admin/auth/signin';
-        } else {
+          return NextResponse.redirect(url);
+        }
+        break;
+      }
+
+      default: {
+        if (!currentPath.startsWith('/admin') && !pathIsPublic) {
+          const url = request.nextUrl.clone();
           url.pathname = '/auth/signin';
+          return NextResponse.redirect(url);
         }
-        const response = NextResponse.redirect(url);
-        response.cookies.set('better-auth.session_token', '', {
-          path: '/',
-          maxAge: 0,
-        });
-        return response;
+        break;
       }
-
-      const { user } = session as unknown as Session;
-
-      const role = user?.role;
-      switch (role) {
-        case 'USER': {
-          if (currentPath.startsWith('/admin') && !pathIsPublic) {
-            const url = request.nextUrl.clone();
-            url.pathname = '/admin/auth/signin';
-            return NextResponse.redirect(url);
-          }
-          break;
-        }
-
-        default: {
-          if (!currentPath.startsWith('/admin') && !pathIsPublic) {
-            const url = request.nextUrl.clone();
-            url.pathname = '/auth/signin';
-            return NextResponse.redirect(url);
-          }
-          break;
-        }
-      }
-    } catch (error) {
-      return NextResponse.next({
-        request,
-      });
     }
   }
 
@@ -122,3 +99,100 @@ export const config = {
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
+
+// import { NextRequest, NextResponse } from 'next/server';
+
+// export async function middleware(request: NextRequest) {
+//   const sessionCookie = getSessionCookie(request, {});
+//   const currentPath = request.nextUrl.pathname;
+//   const pathIsPublic = publicRoutes.find((route) => route === currentPath);
+
+//   const isAuthenticated = sessionCookie != null;
+
+//   if (!isAuthenticated && !pathIsPublic) {
+//     const url = request.nextUrl.clone();
+//     if (currentPath.startsWith('/admin')) {
+//       url.pathname = '/admin/auth/signin';
+//     } else {
+//       url.pathname = '/auth/signin';
+//     }
+//     return NextResponse.redirect(url);
+//   }
+
+//   if (isAuthenticated) {
+//     try {
+//       const { data: session, error } = await betterFetch<
+//         typeof authClient.$Infer.Session
+//       >('/api/better/auth/get-session', {
+//         baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+//         headers: {
+//           cookie: request.headers.get('cookie') || '', // Forward the cookies from the request
+//         },
+//       });
+
+//       if (error) {
+//         console.error('Session fetch error:', error);
+//         return NextResponse.error(); // This triggers the default Next.js error page (500 error)
+//       }
+
+//       // Posible case when user revoke other session, or reset password
+//       if (!session) {
+//         const url = request.nextUrl.clone();
+//         if (currentPath.startsWith('/admin')) {
+//           url.pathname = '/admin/auth/signin';
+//         } else {
+//           url.pathname = '/auth/signin';
+//         }
+//         const response = NextResponse.redirect(url);
+//         response.cookies.set('better-auth.session_token', '', {
+//           path: '/',
+//           maxAge: 0,
+//         });
+//         return response;
+//       }
+
+//       const { user } = session as unknown as Session;
+
+//       const role = user?.role;
+//       switch (role) {
+//         case 'USER': {
+//           if (currentPath.startsWith('/admin') && !pathIsPublic) {
+//             const url = request.nextUrl.clone();
+//             url.pathname = '/admin/auth/signin';
+//             return NextResponse.redirect(url);
+//           }
+//           break;
+//         }
+
+//         default: {
+//           if (!currentPath.startsWith('/admin') && !pathIsPublic) {
+//             const url = request.nextUrl.clone();
+//             url.pathname = '/auth/signin';
+//             return NextResponse.redirect(url);
+//           }
+//           break;
+//         }
+//       }
+//     } catch (error) {
+//       return NextResponse.next({
+//         request,
+//       });
+//     }
+//   }
+
+//   return NextResponse.next({
+//     request,
+//   });
+// }
+
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico (favicon file)
+//      */
+//     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+//   ],
+// };
